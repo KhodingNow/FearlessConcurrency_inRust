4242c72f09d3ee98ac95173ba77936b01bc07a4c
Melezwa26 <phalosenzwa@gmail.com>
2025-05-18 19:43:00 +0200

    update fearless concurrency file

--- src/main.rs
+++ src/main.rs
@@ -48,8 +48,112 @@ fn main() {
         println!("Injalo ke mnumzana {i} osuka eQokolweni");
         thread::sleep(Duration::from_millis(1));
     }
-
    
-//}    
+//}  
+    
+// Passing Data btwn Threads, using Message Passing (Upstream 'tx' to Downstream 'rx' channel analogy )
+
+// Sending Multiple Values & Seeing the Receiver Waiting: EXAMPLE;
+
+    use std::sync::mpsc;
+    use std::thread;
+    use std::time::Duration;
+
+//    fn main() {
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("Molo"),
+                String::from("sisi"),
+                String::from("kunjani?"),
+                String::from("Uphilile na?"),
+                String::from("Bendicela umzuzu..qha!"),
+                String::from("wokuthetha nawe, mna"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));                
+            }
+        });
+
+        for received in rx {
+            println!("Got: {received}");
+        } // this time, the spawned thread has a vector of strings that we want to send to the main thread. We iterate over them,
+        // sending each individually, and pause between each by calling the 'thread::sleep' function with a Duration value of one second.
+        // In the main thread, we're not calling the 'recv' function explicitly anymore: instead, we're treating 'rx' as an iterator. 
+        // For each value received, we're printing it. When the channel is closed, iteration will end. 
+// } 
+
+// Creating Multiple Producers by Clonig the Transmitter
+
+// fn main(() {
+        let (tx, rx) = mpsc::channel();
+
+        let tx1 = tx.clone();
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("Molweni"),
+                String::from("Zidwesha"),
+                String::from("Beku'thwani na?"),
+                String::from("Yaze yathini"),
+                String::from("Injalo ke"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("Yoh yoh yhoo"),
+                String::from("Eyi"),
+                String::from("Bhiza"),                
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        for received in rx {
+            println!("Got: {received}");
+        }
+
+    // Multiple Ownership WITH Multiple Threads using Arc<T>, and Mutex:
+
+    use std::sync::{Arc, Mutex};
+    //use std::thread;
+
+//  fn main() { 
+        let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
+
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+            let handle = thread::spawn(move || {
+                let mut num = counter.lock().unwrap();
+
+                *num += 1;                
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        println!("Result: {}", *counter.lock().unwrap());
+// }
+//}
+
+
+
+
+// }
 
 }
